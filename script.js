107796eb37c68c22c55469be43cb99b44fabfa2f
Nick Overacker <nick.overacker@ymail.com>
2021-11-27 23:29:00 +0900

    Slight rework of buildTruthTable. Much more required later.

--- script.js
+++ script.js
@@ -715,49 +715,54 @@ function computeOutput(inputVals, outputNode) {
 // Z is high impedance, X is error (VDD and GND contradiction.)
 function buildTruthTable() {
     'use strict';
-    let header = [];
-    let inputVals = [];
-    let outputVals = [];
+    let header      = new Array(inputs.length + outputs.length);
+    let inputTable  = new Array(Math.pow(2, inputs.length));
+    let outputTable = new Array(Math.pow(2, inputs.length));
 
     // Each loop iteration is a combination of input values.
     // I.e., one row of the output table.
-    for (let ii = 0; ii < Math.pow(2, inputs.length); ii++) {
-        let tableInputRow = [];
-        let tableOutputRow = [];
+    inputTable.forEach(function(row, rowIndex) {
+        let tableInputRow  = new Array(inputs.length);
+        let tableOutputRow = new Array(outputs.length);
 
         // Compute each output.
-        for (let jj = 0; jj < outputs.length; jj++) {
-            tableOutputRow[jj] = computeOutput(ii, outputNodes[jj]);
-        }
+        tableOutputRow.forEach(function(cell, colIndex) {
+            cell = computeOutput(rowIndex, outputNodes[colIndex]);
+        });
 
-        outputVals[ii] = tableOutputRow;
+        outputTable[rowIndex] = tableOutputRow;
 
-        for (let jj = 0; jj < inputs.length; jj++) {
+        tableInputRow.forEach(function(cell, colIndex) {
             /*jslint bitwise: true */
-            tableInputRow[jj] = (ii >> jj) & 1;
+            cell = (rowIndex >> colIndex) & 1;
             /*jslint bitwise: false */
-        }
+        });
 
-        inputVals[ii] = tableInputRow;
-    }
+        row = tableInputRow;
+    });
 
     // Header
-    for (let jj = inputVals[0].length - 1; jj >= 0; jj--) {
-        header[inputVals[0].length - 1 - jj] = String.fromCharCode(65 + jj);
+    // This needs to run backwards in the current setup.
+    // TODO: This needs to be refactored.
+    for (let jj = inputTable[0].length - 1; jj >= 0; jj--) {
+        header[inputTable[0].length - 1 - jj] = String.fromCharCode(65 + jj);
     }
 
     // Merge input and output into one table (input on the left, output on the right.)
-    let table = [];
-    table[0] = header;
-    table[0][header.length] = "Y";
-    for (let ii = 0; ii < inputVals.length; ii++) {
-        // Reverse the input row.
-        let inputRow = [];
-        for (let jj = 0; jj < inputVals[ii].length; jj++) {
-            inputRow[jj] = inputVals[ii][inputVals[ii].length - 1 - jj];
+    let table = new Array(inputTable.length + 1); // +1 for header.
+    table.forEach(function(row, rowIndex) {
+        if(rowIndex === 0) {
+            row = header;
+            row[header.length] = "Y";
+            return;
         }
-        table[ii + 1] = inputRow.concat(outputVals[ii]);
-    }
+        // Reverse the input row.
+        let inputRow = new Array(inputs.length);
+        inputRow.forEach(function(cell, colIndex) {
+            cell = inputTable[rowIndex - 1][inputTable[rowIndex - 1].length - 1 - colIndex];
+        });
+        row = inputRow.concat(outputTable[rowIndex - 1]);
+    });
 
     return table;
 }
