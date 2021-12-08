dffaecc333f3f973fc3081f8124a2d1a22b555bc
Nick Overacker <nick.overacker@ymail.com>
2021-12-08 23:06:12 +0900

    Fix accessible mode toggle

--- script.js
+++ script.js
@@ -1273,7 +1273,7 @@ class DiagramController {
             if (event.shiftKey) {
                 // Toggle accessible mode by pressing A.
                 if (event.keyCode === 65) {
-                    this.diagram.accessibleMode = !this.diagram.accessibleMode;
+                    this.diagram.accessible = !this.diagram.accessible;
                     setUpLayerSelector();
                 }
 
