04ba415db9a8cff3b2ff6795a969ad6981d94ca0
Nick Overacker <nick.overacker@ymail.com>
2021-12-12 19:30:58 +0900

    Eliminate backgroundEval. Doesn't work, unnecessary.

--- script.js
+++ script.js
@@ -115,32 +115,6 @@ class Diagram {
         this.controller = new DiagramController(this, this.view, mainCanvas);
     }
 
-    evaluateAll() {
-        'use strict';
-        /*
-        this.outputNodes.some(function(node) {
-            let node1Index = this.graph.getIndexByNode(node);
-
-            for(let ii = 0; this.controller.backgroundEval && ii < this.analyses.length; ii++) {
-                this.analyses[ii][node1Index].some(function(mapping, node2Index) { 
-                    if(mapping !== true && mapping !== false) {
-                        if(!this.controller.backgroundEval) {
-                            return true;
-                        }
-                        this.nodeNodeMap = [... this.analyses[ii],];
-                        this.computeOutputRecursive(node, this.graph.nodes[node2Index], ii);
-                        this.analyses[ii] = [... this.nodeNodeMap,];
-                    }
-                }.bind(this));
-            }
-
-            if(!this.controller.backgroundEval) {
-                return true;
-            }
-        }.bind(this));*/
-        this.controller.backgroundEval = false;
-    }
-
     // Helps with garbage collection
     clearAnalyses() {
         'use strict';
@@ -891,7 +865,6 @@ class DiagramController {
         this.placeTermMode    = false;
         this.selectedTerminal = null;
         this.shiftCommands    = [];
-        this.backgroundEval   = false;
 
         // Set up shift commands
         this.shiftCommands[37] = ((e) => {
@@ -948,14 +921,6 @@ class DiagramController {
                 this.placeTerminal(e, this.diagram.vddCell);
             }
         }).bind(this);
-    }
-
-    // Call this when the state of the canvas changes,
-    // rendering the truth table potentially incorrect.
-    stopBackgroundEval() {
-        'use strict';
-        this.backgroundEval = false;
-        document.getElementById("truth-table").innerHTML = "";
     }
 
     removeTerminal(isOutput) {
@@ -1192,14 +1157,12 @@ class DiagramController {
         let coords = this.getCoordsFromEvent(event);
 
         if(this.placeTermMode) {
-            this.stopBackgroundEval();
             this.clearPlaceTerminalMode();
             this.placeTerminal(event, this.selectedTerminal);
         } else if (!this.isEraseEvent(event)) {
             // Just fill in or delete the cell at the start coordinates.
             // If there is no cell at the start coordinates, change the cursor color.
             if (!this.diagram.layeredGrid.get(this.startX, this.startY, this.cursorIndex).isSet) {
-                this.stopBackgroundEval();
                 this.saveCurrentState();
                 this.diagram.layeredGrid.set(this.startX, this.startY, this.cursorIndex);
             }
@@ -1315,9 +1278,6 @@ class DiagramController {
             this.undo();
             return;
         }
-
-        // Kill background evaluation if it is running.
-        this.stopBackgroundEval();
 
         let endX = Math.floor((currentX - this.view.canvas.getBoundingClientRect().left - this.view.cellWidth) / this.view.cellWidth);
         let endY = Math.floor((currentY - this.view.canvas.getBoundingClientRect().top - this.view.cellHeight) / this.view.cellHeight);
@@ -2348,10 +2308,6 @@ function refreshTruthTable(suppressSetNets) {
     });
 
     window.scrollTo({behavior: "smooth", top: Math.ceil(tableElement.getBoundingClientRect().top + window.scrollY), left: 0,});
-
-    // Bare minimum complete -- now continue to evaluate additional paths in the background.
-    // This will continue until either the canvas state changes or all output nets have been evaluated.
-    diagram.evaluateAll();
 }
 
 function setDarkMode(setToDark) {
