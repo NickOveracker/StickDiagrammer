d36b7456e8f55d72ff300f6a242e22be57ca0b74
Nick Overacker <nick.overacker@ymail.com>
2022-12-28 20:27:02 +0900

    Fixed the retrieval of the gate node.

--- script.js
+++ script.js
@@ -523,9 +523,7 @@ class Diagram {
         // If the gate is an input, the gate's state depends on the input value.
         if (gateNet.isInput) {
             let inputNum, tempEval, evalInput;
-            window.gatenet = gateNet;
-            console.log(gateNet);
-            let gateNode = this.graph.getNode(gateNet);
+            let gateNode = gateNet.nodes.entries().next().value;
 
             for(let ii = 0; ii < this.inputs.length; ii++) {
                 if(!gateNode.isConnected(this.inputNodes[ii])) {
