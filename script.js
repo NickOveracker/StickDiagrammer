87f6245845aafa192d963edc7fda18883d398e38
Nick Overacker <nick.overacker@ymail.com>
2023-02-01 00:13:00 +0900

    Need to actually pass the event to getCoordsFromEvent()

--- script.js
+++ script.js
@@ -508,7 +508,7 @@
         endDrag(event) {
             let canvas = this.diagramView.canvas;
             let view   = this.diagramView;
-            let coords = this.diagramController.getCoordsFromEvent();
+            let coords = this.diagramController.getCoordsFromEvent(event);
 
             // If the mouse was released outside the canvas, undo and return.
             if(!this.diagramController.pixelIsInBounds()) {
